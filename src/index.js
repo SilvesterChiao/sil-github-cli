f640546f9369276d5dd8894f38f17651acef903d
SilvesterChiao <SilvesterChiao@gmail.com>
2019-10-05 00:23:39 +0800

    feat：命令行查看github仓库

--- src/index.js
+++ src/index.js
@@ -1,65 +1,51 @@
 #!/usr/bin/env node
-// #!/usr/bin/node
+
 const readline = require('readline');
+const program = require('commander');
 const request = require('request');
+const colors = require('colors');
+const ora = require('ora');
 
-var colors = require('colors');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+program
+  .version('0.0.1')
+  .option('--repos', 'view repos')
+  .parse(process.argv);
 
-rl.question('你想对谁说声hello？ '.red, answer => {
-  getRepository(answer);
-  //   var e = request(
-  //     {
-  //       url: 'https://api.github.com/users/SilvesterChiao/repos',
-  //       method: 'GET',
-  //       headers: { 'Content-Type': 'text/json' },
-  //     },
-  //     function(error, response, body) {
-  //       if (!error && response.statusCode == 200) {
-  //         console.log(response);
-  //         // res.render('task',{'data':JSON.parse(body) } );
-  //       }
-  //     },
-  //   );
-});
+if (program.repos) {
+  const userName = process.argv[3];
+  getRepos(userName);
+}
 
-function getRepository(userName) {
+function getRepos(userName) {
+  const spinner = ora({
+    text: 'Loading'.yellow,
+  }).start();
+  const optons = {
+    url: `https://api.github.com/users/${userName}/repos`,
+    method: 'GET',
+    json: true,
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+    },
+  };
+
   return new Promise(function(resolve, reject) {
-    const url = `https://api.github.com/users/${userName}/repos`;
-    request(url, function(error, response, body) {
-      console.log(response.statusCode);
-      if (!error && response.statusCode == 200) {
-        const { data } = JSON.parse(body);
-        if (data && data.length !== 0) {
-          data.map(item => {
-            console.log(`Name: ${item.name}`);
-            // console.log(`URL: ${item.URL}`);
-          });
-        }
-      }
-    });
-    rl.close();
+    var requestC = request.defaults({ jar: true });
 
-    // request(
-    //   {
-    //     url: `https://api.github.com/users/${userName}/repos`,
-    //     method: 'GET',
-    //     json: true,
-    //     headers: {
-    //       'User-Agent':
-    //         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
-    //     },
-    //   },
-    //   function(error, response, body) {
-    //     if (error) {
-    //       console.log('error occurred: ' + error);
-    //       reject(error);
-    //     }
-    //     console.log(body);
-    //   },
-    // );
+    requestC(optons, function(error, response, body) {
+      if (error) {
+        spinner.fail('Fail'.red);
+        console.log('error occurred: ' + error);
+        reject(error);
+        process.exit();
+      }
+      spinner.succeed('Succeed'.green);
+      console.log(colors.magenta(`用户${userName}的 public 仓库列表；`));
+      body.forEach((item, index) => {
+        console.log(colors.blue(`${index + 1}: ${item.name}`));
+      });
+      process.exit();
+    });
   });
 }
